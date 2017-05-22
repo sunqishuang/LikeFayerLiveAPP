43d8885bec585738568d699e0787eb3414c61d36
sunqishuang <1244511273@qq.com>
2017-05-22 15:21:25 +0800

    live
    upload EmptyProject

---
+++ LikeFlyerLiveAPP/LikeFlyerLiveAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LikeFlyerLiveAPP
+//
+//  Created by zxy on 2017/5/22.
+//  Copyright © 2017年 SunQishuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LikeFlyerLiveAPP/LikeFlyerLiveAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LikeFlyerLiveAPP
+//
+//  Created by zxy on 2017/5/22.
+//  Copyright © 2017年 SunQishuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
